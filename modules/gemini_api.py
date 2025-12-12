417af7b45de94cd68a99e271f02ab381c76d9105
Naman Negi <159187869+nmnnegi@users.noreply.github.com>
2025-12-12 13:06:56 +0530

    feat: model change

--- modules/gemini_api.py
+++ modules/gemini_api.py
@@ -11,6 +11,6 @@ if not api_key:
 genai.configure(api_key=api_key)
 
 def get_gemini_response(input_text, pdf_content, prompt):
-    model = genai.GenerativeModel('gemini-2.5-pro')
+    model = genai.GenerativeModel('gemini-2.5-flash')
     response = model.generate_content([input_text, pdf_content[0], prompt])
     return response.text
